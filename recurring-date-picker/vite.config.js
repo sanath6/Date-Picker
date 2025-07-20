865e85d56e18a8c3b8275d9a2968de0ae03a7ed6
Sanath Kumar Etyala <118047859+sanath6@users.noreply.github.com>
2025-07-21 01:16:31 +0530

    Vite.config.js changes - Added server.allowedHosts property

--- recurring-date-picker/vite.config.js
+++ recurring-date-picker/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['date-picker-wc4s.onrender.com'], // ✅ add your Render domain here
+  }
 })
